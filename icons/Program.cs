4b2d3449d7ed0f49c147b948da75fc5dc77a9563
aflegel <alex.g.flegel@gmail.com>
2018-02-01 13:02:08 +0900

    established the basic svgs for compilation

---
+++ icons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace icons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
