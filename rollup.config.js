1b44af13ed0aa4705a789656bb82279984b31bbe
Alexander Boutin Flegel <alexander.boutinflegel@propellerhead.co.nz>
2021-04-04 07:55:02 +1200

    rollup plugins for typescript and svgs resolved tsx files

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,10 @@ import babel from "@rollup/plugin-babel";
 import external from "rollup-plugin-peer-deps-external";
 import del from "rollup-plugin-delete";
 import pkg from "./package.json";
+import typescript from "@rollup/plugin-typescript";
+import svg from "rollup-plugin-svg";
+
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
 
 export default {
   input: pkg.source,
@@ -10,9 +14,12 @@ export default {
     { file: pkg.module, format: "esm" },
   ],
   plugins: [
+    svg(),
+    typescript(),
     external(),
     babel({
       exclude: "node_modules/**",
+      extensions,
     }),
     del({ targets: ["dist/*"] }),
   ],
---
+++ src/Components/AdvantageIcon.tsx
@@ -0,0 +1,5 @@
+import svg from "../svg/swrpg-advantage.svg";
+
+export const AdvantgeIcon = (props: { className: string }) => {
+  return <image src={svg}></image>;
+};
